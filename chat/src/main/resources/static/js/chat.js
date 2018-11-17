a9f8e17d090ad3303760224bfb0816880c63a6bd
den3011den <den3011den@gmail.com>
2018-11-18 00:37:09 +0700

    Правка сраницы чата

--- chat/src/main/resources/static/js/chat.js
+++ chat/src/main/resources/static/js/chat.js
@@ -59,7 +59,7 @@ $( document ).ready(function() {
                  });
              },
             error : function(e) {
-                  $("#postResultDiv").html("<div style='background-color:#7FA7B0; color:white; padding:20px 20px 20px 20px'>" +
+                  $("#postResultDiv").html("<div class='postResultDiv'>" +
                           "Не удалось получить данные с сервера <br>" + "</div>");
             }
         });
@@ -70,5 +70,56 @@ $( document ).ready(function() {
         ajaxAskNewMessages;
         setTimeout(run, 5000);
     }, 5000);*/
+
+});
+
+$( document ).ready(function() {
+
+    // SUBMIT FORM
+    $("#sendform").submit(function(event) {
+        // Prevent the form from submitting via the browser.
+        event.preventDefault();
+        ajaxPost();
+    });
+
+
+    function ajaxPost(){
+
+        // PREPARE FORM DATA
+        var formData = {
+            login : $("#login").val(),
+            message :  $("#message").val()
+        };
+
+        $.ajax({
+            type : "POST",
+            contentType : "application/json",
+            url : "/sendmessage",
+            data : JSON.stringify(formData),
+            dataType : 'json',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            success: function(jsondata) {
+                if (jsondata.success == "true") {
+                    resetData();
+                    $("#postResultDiv").html("<p></p>");
+                }
+                else  {
+                    $("#postResultDiv").html("<div id='postResultDiv' class='postResultDiv'>" +
+                        "Не удалось отправить сообщение! Сервер получил данные, но не смог сохранить их в БД <br>" + "</div>");
+                }
+            },
+            error : function(e) {
+                $("#postResultDiv").html("<div id='postResultDiv' class='postResultDiv'>" +
+                    "Не удалось отправить сообщение! Возможно сервер не доступен. <br>" + "</div>");
+            }
+        });
+    };
+
+    function resetData(){
+        $("#message").val("");
+    }
 
 });
